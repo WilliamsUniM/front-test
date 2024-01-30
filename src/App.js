13bcf448c6f1808592208ef3a4807ecb03d3655f
WilliamsUniM <williams.maldonado@uniminuto.edu.co>
2024-01-29 23:46:43 -0500

    Config github pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Poke from './components/poke';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Poke />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Poke />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
