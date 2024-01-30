3dc3431ab92043544c51158272619dbfebb7b967
WilliamsUniM <williams.maldonado@uniminuto.edu.co>
2024-01-29 23:13:49 -0500

    Config github pages

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,7 @@ export default class Header extends Component {
         return (
             <header className='d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom'>
                 <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                    <img src={`${process.env.PUBLIC_URL}/W.png`} style={{ width: 3 + '%' }} />
+                    <img src={`${process.env.PUBLIC_URL}/W.png`} style={{ width: 3 + '%' }} alt='logo'/>
                     <h2>Williams Alexander Maldonado Pardo</h2>
                 </a>
             </header>
